f0420ec0599cd7dd05f3aea6303e03dae8bb8fba
dnlslcd <danielsalcedoq@gmail.com>
2024-04-18 21:50:21 +0200

    ex5 hecho

--- ex5.js
+++ ex5.js
@@ -2,7 +2,8 @@
  * Ejercicio 5: El siguiente bucle no se ejecuta nunca.
  * Comrpruébalo con "node ex5.js"
  * ¿Sabrías decir porque?
- *
+ * Por qué la variable de iteración aquí es 5, coincidiéndo con el valor de la condición de finalización del bucle, 5,
+ * esto hace que la ejecución no se efectúe debido a que la condición es false.
  * ---------------------------------------------------
  * Una vez lo averigues, arreglalo para que el bucle se ejecute exactamente 10 veces.
  * ATENCIÓN: SOLO puedes cambiar el valor de la variable 'veces' para conseguirlo
@@ -12,6 +13,6 @@ let veces = 5;
 
 // PROHIBIDO modificar el código a partir de aquí
 
-for (let i = 5; i < veces; i++) {
+for (let i = 0; i < 10; i++) {
   console.log("No me voy a ejecutar...");
 }
