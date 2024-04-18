b586bb7f2305bc05e1e99c499c35138f795c1a8f
dnlslcd <danielsalcedoq@gmail.com>
2024-04-18 21:25:37 +0200

    ex2 hecho

--- ex2.js
+++ ex2.js
@@ -2,7 +2,9 @@
  * Ejercicio 2: Escribe un bucle, parecido al del ejercicio 1,
  * que muestre por el terminal el mensaje "Yo también estoy en un bucle!" ; un total de 20 veces
  */
-
+for (i = 0; i < 20; i++) {
+    console.log("Yo también estoy en un bucle!");
+}
 /**
  * Resultado esperado: https://oscarm.tinytake.com/msc/ODc4MzU2M18yMjIwMzc5Ng
  */
